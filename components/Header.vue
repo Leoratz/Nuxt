02b9cc7b0be93f4e1f460c057aced95b17fc1009
Léora Chriqui <leora.chriqui@gmail.com>
2024-12-13 11:51:51 +0100

    adding components

---
+++ components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <header class="bg-blue-500 text-white py-4">
+      <div class="container mx-auto flex justify-between items-center">
+        <h1 class="text-xl font-bold">Jetpunk</h1>
+        <nav>
+          <NuxtLink to="/" class="text-white hover:underline">Accueil</NuxtLink>
+        </nav>
+      </div>
+    </header>
+</template>
